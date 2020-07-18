4862de1fee9d4708cb4e912a0ebdb2ab2ac98dcc
juniordugue <jrdugue@gmail.com>
2020-07-18 04:01:46 -0400

    fixed bug in charactergrid displaying isLoading on the screen

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -2,14 +2,12 @@ import React from 'react'
 import CharacterItem from "./CharacterItem";
 
 const CharacterGrid = ({items, isLoading}) => {
-    return (
-        <div>
-            isLoading ? <h1>Loading...</h1> : <section className="cards">
+    return isLoading ? (
+             <h1>Loading...</h1> ) : ( <section className="cards">
             {items.map(item => (
                 <CharacterItem key={item.char_id} item={item}></CharacterItem>
             ))}
             </section>
-        </div> 
     )
 }
 
